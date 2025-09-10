1109734bab1baf6c0e161a77feb94cc9623a30de
Blastor99 <lgilposada@byupathway.edu>
2025-09-10 01:01:24 -0600

    syntax error solved

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,8 +3,7 @@ const today = new Date();
 year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 
 
-const displayElement = document.getElementById("lastModified");
-displayElement.textContent = ` ${new Date().toDateString()}`;
+c
 
 
 
